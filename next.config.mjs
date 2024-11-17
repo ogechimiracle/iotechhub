495e90114e86ba1771852037a4c88d39984733de
iotechhub <iotechhub@gmail.com>
2024-11-17 21:10:38 +0100

    added back _redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    images: {
+        domains: ['iotechhub.netlify.app'], // Allow the Netlify domain (if using relative URLs)
+      },
 };
 
 export default nextConfig;
